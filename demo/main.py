32db2165b6e8ad889dbba95f3d95bcc1400105b4
jmossorio99 <joseossorio99@hotmail.com>
2023-03-26 16:38:41 -0700

    progress

--- demo/main.py
+++ demo/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from qplex.library.qmodel import QModel
+from itertools import product
 import re
 
 
@@ -10,7 +12,13 @@ def build_demo_df():
     column_list = data['Name'].unique()
     temp = set_columns(data, temp, column_list)
     refactored_df = pd.DataFrame(columns=temp.columns.tolist())
-    years = ['2013-02', '2013-03', '2013-04', '2013-05', '2013-06', '2013-07', '2013-08', '2013-09', '2013-10', '2013-11', '2013-12', '2014-01', '2014-02', '2014-03', '2014-04', '2014-05', '2014-06', '2014-07', '2014-08', '2014-09', '2014-10', '2014-11', '2014-12', '2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06', '2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12', '2016-01', '2016-02', '2016-03', '2016-04', '2016-05', '2016-06', '2016-07', '2016-08', '2016-09', '2016-10', '2016-11', '2016-12', '2017-01', '2017-02', '2017-03', '2017-04', '2017-05', '2017-06', '2017-07', '2017-08', '2017-09', '2017-10', '2017-11', '2017-12', '2018-01']
+    years = ['2013-02', '2013-03', '2013-04', '2013-05', '2013-06', '2013-07', '2013-08', '2013-09', '2013-10',
+             '2013-11', '2013-12', '2014-01', '2014-02', '2014-03', '2014-04', '2014-05', '2014-06', '2014-07',
+             '2014-08', '2014-09', '2014-10', '2014-11', '2014-12', '2015-01', '2015-02', '2015-03', '2015-04',
+             '2015-05', '2015-06', '2015-07', '2015-08', '2015-09', '2015-10', '2015-11', '2015-12', '2016-01',
+             '2016-02', '2016-03', '2016-04', '2016-05', '2016-06', '2016-07', '2016-08', '2016-09', '2016-10',
+             '2016-11', '2016-12', '2017-01', '2017-02', '2017-03', '2017-04', '2017-05', '2017-06', '2017-07',
+             '2017-08', '2017-09', '2017-10', '2017-11', '2017-12', '2018-01']
     year_index = 0
     years_len = len(years)
     for index in temp.index:
@@ -35,16 +43,37 @@ def set_columns(data, df, cols):
     return refactored_df
 
 
-def get_data(budget):
+def get_data(b):
     # build_demo_df()
     data = pd.read_csv('data/demo_data.csv', index_col=0)
     # print(data)
-    max_num_shares = (budget / data.iloc[-1]).astype(int)
+    max_num_shares = (b / data.iloc[-1]).astype(int)
     price = data.iloc[-1]
     monthly_returns = data[data.columns.values.tolist()].pct_change().iloc[1:]
     avg_monthly_returns = monthly_returns.mean(axis=0)
     covariance_matrix = monthly_returns.cov()
-    return max_num_shares, price, avg_monthly_returns, covariance_matrix
+    return max_num_shares, price, avg_monthly_returns, covariance_matrix, data.columns.tolist()
 
 
-max_num_shares, price, avg_monthly_returns, covariance_matrix = get_data(1000)
+# Define budget
+budget = 100
+# Define max_risk
+max_risk = 50
+
+max_num_shares, price, avg_monthly_returns, covariance_matrix, stocks = get_data(budget)
+
+# Define n -> number of variables
+n = len(stocks)
+# Create model
+portfolio_model = QModel('portfolio')
+# Define variable list
+x = portfolio_model.integer_var_list(n, name=lambda index: stocks[index])
+# Adding constraint: cost <= budget
+cost = sum(price[s] * x[index] for index, s in enumerate(stocks))
+portfolio_model.add_constraint(cost <= budget)
+# Adding constraint: risk <= max_risk
+risk = 0
+# Make a nested for loop
+for s1, s2 in product(stocks, stocks):
+    coefficient = covariance_matrix[s1][s2] * price[s1] * price[s2]
+    risk = risk + coefficient * x[s1] * x[s2]
